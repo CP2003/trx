8613f014c866445f78698acf8d817f8145d1c380
CP2003 <59545375+CP2003@users.noreply.github.com>
2022-12-05 23:28:06 +0530

    Add files via upload

---
+++ plugins/_amdi_menu.js
@@ -0,0 +1,52 @@
+/**
+░██████╗░██╗░░░██╗███████╗███████╗███╗░░██╗  ░█████╗░███╗░░░███╗██████╗░██╗
+██╔═══██╗██║░░░██║██╔════╝██╔════╝████╗░██║  ██╔══██╗████╗░████║██╔══██╗██║
+██║██╗██║██║░░░██║█████╗░░█████╗░░██╔██╗██║  ███████║██╔████╔██║██║░░██║██║
+╚██████╔╝██║░░░██║██╔══╝░░██╔══╝░░██║╚████║  ██╔══██║██║╚██╔╝██║██║░░██║██║
+░╚═██╔═╝░╚██████╔╝███████╗███████╗██║░╚███║  ██║░░██║██║░╚═╝░██║██████╔╝██║
+░░░╚═╝░░░░╚═════╝░╚══════╝╚══════╝╚═╝░░╚══╝  ╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═════╝░╚═╝
+ __  __       _ _   _       ____             _          
+|  \/  |_   _| | |_(_)     |  _ \  _____   _(_) ___ ___ 
+| |\/| | | | | | __| |_____| | | |/ _ \ \ / / |/ __/ _ \
+| |  | | |_| | | |_| |_____| |_| |  __/\ V /| | (_|  __/
+|_|  |_|\__,_|_|\__|_|     |____/ \___| \_/ |_|\___\___|
+* @project_name Queen Amdi [WA Multi-device]
+* @author BlackAmda <https://github.com/BlackAmda>
+* @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
+* @link <https://github.com/BlackAmda/QueenAmdi>
+* @version 4.0.1
+* @file  _amdi_menu.js - QueenAmdi bot main menu
+
+© 2022 Black Amda, ANTECH. All rights reserved.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.*/
+
+const { AMDI, amdiDB, _default, _default_list_sections, Language } = require('queen_amdi_core/dist/scripts');
+const { getSettings } = amdiDB.settingsDB
+const { amdiVoice } = _default
+const { panelList } = _default_list_sections
+const Lang = Language.getString('amdiMenu');
+
+/**
+ * @cmdInfos { cmd, desc, example, type, react, cmdHideInMenu }
+ * @cmdTypes primary, download, logo, profile, admin
+*/
+
+AMDI({ cmd: ["panel", "test", "list", "menu"], desc: "Queen Amdi Main Menu", type: "primary", react: "✅", cmdHideInMenu: true }, (async (amdiWA) => {
+    let { prefix, sendAudioMsg, sendListMsg, msgDevice, sendername } = amdiWA.msgLayout;
+   
+    const audioURL = amdiVoice
+    const pttStatus = true
+    let mimeType = msgDevice == 'ios' ? 'audio/mp4' : 'audio/ogg; codecs=opus'
+    await sendAudioMsg({ url: audioURL }, {mimetype: mimeType, ptt: pttStatus});
+    const PANEL_HEADER = await getSettings('PANEL_HEADER');
+    let text = !PANEL_HEADER.input || PANEL_HEADER.input == 'default' ? `\n*Hello!* ${sendername}` + Lang.panelText : PANEL_HEADER.input.keywords();
+
+    var listInfo = {}
+    listInfo.title = Lang.panelTitle
+    listInfo.text = text
+    listInfo.buttonTXT = 'default'
+
+    const sections = panelList(prefix);
+    return await sendListMsg(listInfo, sections);
+}));
